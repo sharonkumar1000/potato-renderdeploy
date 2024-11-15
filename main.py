c1f6b1a57ebf8ec951d2e4eec8cf01cec2900d69
pala sharon kumar <163455102+sharonkumar1000@users.noreply.github.com>
2024-11-15 19:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# from fastapi import FastAPI,File,UploadFile
-# import uvicorn
-# import numpy as np
-# from io import BytesIO
-# from PIL import Image
-#
-# app = FastAPI()
-#
-# @app.get("/ping")
-#
-# async def ping():
-#     return "Hello, Iam alive"
-#
-# @app.post("/predict")
-#
-# def read_file_as_image(data)-> np.ndarray:
-#     image = np.array(Image.open(BytesIO(data)))
-#     return image
-#
-# async def predict(
-#         file:UploadFile = File(...)
-# ):
-#     image = read_file_as_image(await file.read())
-#     pass
-#
-# if __name__ == "__main__":
-#     uvicorn.run(app,host = 'localhost',port = 8000)
-
 
 
 from fastapi import FastAPI, File, UploadFile
